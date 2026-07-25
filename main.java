7d1d78613e7f9a3fce4d2b0784ce7e4cceb0adee
abdur2695 <abdurrashid260905@gmail.com>
2026-07-25 13:33:22 +0530

    Refactor main method to use LoginFrame
    Removed order processing logic and integrated LoginFrame.

--- main.java
+++ main.java
@@ -1,34 +1,15 @@
 package main;
 
-import dao.OrderDAO;
-import model.Order;
-
-import java.time.LocalDateTime;
+import javax.swing.SwingUtilities;
+import ui.LoginFrame;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        OrderDAO dao = new OrderDAO();
-
-        Order order = new Order();
-
-        order.setUserId(1);
-        order.setTotal(450);
-        order.setOrderDate(LocalDateTime.now());
-
-        int orderId = dao.placeOrder(order);
-
-        if(orderId != -1){
-
-            System.out.println("Order Placed Successfully");
-            System.out.println("Order ID : " + orderId);
-
-        }else{
-
-            System.out.println("Order Failed");
-
-        }
+        SwingUtilities.invokeLater(() -> {
+            new LoginFrame();
+        });
 
     }
 }
